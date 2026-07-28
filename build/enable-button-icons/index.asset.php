2cbd18e9b3d58349b42dc6a0295dcc78a37c1fb6
Morgan Leek <morganleek@gmail.com>
2026-07-28 09:14:47 +0800

    svg max size

--- build/enable-button-icons/index.asset.php
+++ build/enable-button-icons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '24cfe759a33f1467fa5d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '97b6e0bce21184ff452a');
